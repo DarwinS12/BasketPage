3decf24d3bd3d311e7042fc6ecb8d16623aa33e4
RobertGarciaM <rhernan.garcia@udea.edu.co>
2024-01-05 22:08:39 -0500

    Update Music.jsx

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -4,7 +4,7 @@ import { ButtonsControler } from "./ButtonsControler";
 
 import { VolumeControler } from "./VolumeControler";
 
-export function Music({
+export function Music({ // siempre es bueno que definas el tipo de las propiedades, asi evitas errores, aca estan como any, pero es bueno que si es string las definas tipo string o si es entero etc
   songRef,
   currentSong,
   songToPlay,
